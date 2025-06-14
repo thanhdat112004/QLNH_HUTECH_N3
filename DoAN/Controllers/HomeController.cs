35c266b59853c9fad4bebb1c5905e88cdd99a6a2
OliverFang™ <datom112004@gmail.com>
2025-06-14 19:12:57 +0700

    clone giao dien chinh

---
+++ DoAN/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using DoAN.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoAN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult menu()
+        {
+            return View();
+        }
+        public IActionResult book()
+        {
+            return View();
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+      
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
